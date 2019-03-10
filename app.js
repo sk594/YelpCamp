dbcc35c0cd3ac05b84cb4bb082217f7ed2af4c9e
sanjay gurjar <skgurjar594@gmail.com>
2019-03-10 20:53:28 +0000

    add mongodb

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoute       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb+srv://sanjay:sanjay@cluster0-mbgow.mongodb.net/test?retryWrites=true");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
